9b5b13a0ed42ff085e863c074b6170d2b7c958cf
KimHyuenDeuk <152377864+KimHyuenDeuk@users.noreply.github.com>
2024-01-19 18:07:38 +0900

    Add files via upload

---
+++ TodoCardServiceImplTest.kt
@@ -0,0 +1,59 @@
+package com.teamsparta.todo.todocards
+
+import com.teamsparta.todo.users.User
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.mockk.every
+import io.mockk.mockk
+import org.springframework.data.repository.findByIdOrNull
+
+class TodoCardServiceImplTest : BehaviorSpec({
+    val todoCardRepository = mockk<TodoCardRepository>()
+    val savedTodoCardId = 1L
+    val notSavedTodoCardId = 10L
+    val savedTodoCard = TodoCard(
+        id = savedTodoCardId,
+        title = "title",
+        content = "content",
+        author = User(1, "username", "password"),
+        replies = emptyList(),
+    )
+    every { todoCardRepository.findByIdOrNull(savedTodoCardId) } returns savedTodoCard
+    every { todoCardRepository.findByIdOrNull(notSavedTodoCardId) } returns null
+
+    val service = TodoCardServiceImpl(
+        todoCardRepository
+    )
+
+    Given("a saved todo card id") {
+        val targetTodoCardId = savedTodoCardId
+
+        When("execute findById") {
+            val result = service.findById(targetTodoCardId)
+
+            Then("result should not be null") {
+                result shouldNotBe null
+                result?.let {
+                    it.id shouldBe savedTodoCard.id
+                    it.title shouldBe savedTodoCard.title
+                    it.content shouldBe savedTodoCard.content
+                    it.authorName shouldBe savedTodoCard.author.username
+                    it.replies shouldBe savedTodoCard.replies
+                }
+            }
+        }
+    }
+
+    Given("a not saved todo card id") {
+        val targetTodoCardId = notSavedTodoCardId
+
+        When("execute findById") {
+            val result = service.findById(targetTodoCardId)
+
+            Then("result should be null") {
+                result shouldBe null
+            }
+        }
+    }
+})
